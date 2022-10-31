97a79afa61d1ab0372cf04ddd5691eeb968d8eac
Edson Pimenta <edson.tibo@gmail.com>
2022-10-31 19:17:12 -0300

    Anotações sobre instruções e ponto e vírgula

---
+++ curso_php/basics/instrucao/ponto_e_virgula.php
@@ -0,0 +1,13 @@
+<?php
+
+// Toda instrução termina com ponto e virgula (salvo exceções)
+
+$name = 'Edson';
+
+if ($name === 'Edson') {
+    echo 'É Edson.';
+}
+
+// A última instrução não precisa de ponto e vírgula,
+// Mas só se estivermos fechando o bloco de código php
+echo 'teste';
