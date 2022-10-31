caa12595542b1a0b48d8f0541c6fca5af4eccc5f
Edson Pimenta <edson.tibo@gmail.com>
2022-10-31 19:09:08 -0300

    Anotações sobre Constantes

---
+++ curso_php/basics/constantes/constantes.php
@@ -0,0 +1,27 @@
+<?php
+
+// Para declarar constantes usamos define
+
+//define('NAME', 'Edson');
+//echo NAME;
+
+//if (defined('NAME')) {
+//    echo 'foi definido';
+//}
+
+
+// Constantes pré-definidas
+
+//function teste()
+//{
+//    echo __FUNCTION__;
+//    echo __METHOD__;
+//}
+
+//teste();
+
+
+// Para chamarmos todas as constantes mágicas
+
+//$cons = get_defined_constants(true);
+//var_dump($cons);
