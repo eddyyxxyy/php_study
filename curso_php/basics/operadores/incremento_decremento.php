0ff15a5e5c89853a8f627f4d3929c406b550a6fc
Edson Pimenta <edson.tibo@gmail.com>
2022-10-31 20:16:56 -0300

    Anotações sobre operadores de incremento/decremento

---
+++ curso_php/basics/operadores/incremento_decremento.php
@@ -0,0 +1,18 @@
+<?php
+
+// Operadores de incremento e decremento
+
+$number = 10;
+
+// O incremento altera o valor da variável.
+// ++ soma um, é o incremento.
+echo ++$number; // pré-incremento
+echo $number++; // pós-incremento
+
+// -- subtrai um, é o decremento
+echo --$number; // pré-decremento
+echo $number--; // pós-decremento
+
+// Nesse caso:
+// O pré incremento/decremento altera a variável antes de ser mostrada
+// Já o pós incremento/decremento só altera o valor depois de ter mostrado
