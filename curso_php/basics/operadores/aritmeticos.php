a174fe7b6dcd4d05156a8c59c888a6fa300706b1
Edson Pimenta <edson.tibo@gmail.com>
2022-10-31 20:10:04 -0300

    Anotações sobre operadores aritméticos

---
+++ curso_php/basics/operadores/aritmeticos.php
@@ -0,0 +1,27 @@
+<?php
+
+// Por exemplo, se tivermos:
+
+$number1 = 10;
+$number2 = 20;
+
+// soma
+//echo $number1 + $number2;
+
+// subtração
+//echo $number2 - $number1;
+
+// multiplicação
+//echo $number1 * $number2;
+
+// divisão
+//echo $number1 / $number2;
+
+// resto
+//echo $number1 % $number2;
+
+
+// Podemos atribuir resultados com as operações
+
+$resultado = $number1 + $number2;
+echo $resultado;
