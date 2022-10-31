6fe6a13783f1a4aab68f5af0e4701b46fc76d5e2
Edson Pimenta <edson.tibo@gmail.com>
2022-10-31 16:15:40 -0300

    Anotações sobre variáveis e tipos de dados

---
+++ curso_php/basics/variaveis_e_seus_tipos/variaveis.php
@@ -0,0 +1,46 @@
+<?php
+
+// Data Types
+
+// strings
+//echo gettype('residua$55ç~;.;.,´sa~12´32[1~~');
+
+// numbers - integers - floats(double)
+//echo gettype(12);
+//echo gettype(12.34);
+
+// booleans
+//echo gettype(true);
+//echo gettype(false);
+
+// arrays
+//echo gettype(['Edson', 12]);
+
+// objects
+//class Person
+//{
+//}
+//
+//echo gettype(new Person);
+
+// null
+//echo gettype(null);
+
+
+// Variables
+
+// Toda variável deve começar com letras ou underline
+
+//$name = 'Edson';
+//$_number = 34.99;
+
+// Usualmente utilizamos snake_case ou camelCase
+// PHP é case sensitive
+
+// Ao atribuir o valor de uma variável com outra:
+
+//$my_name = $name;
+
+// Só criamos uma referência se utilizarmos &:
+
+//$my_name = &$name;
