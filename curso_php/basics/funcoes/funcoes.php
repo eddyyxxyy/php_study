c6591745c5cd6171de02df6c375e7893ef6fd36e
Edson Pimenta <edson.tibo@gmail.com>
2022-11-01 14:16:48 -0300

    Anotações e exemplos sobre funções

---
+++ curso_php/basics/funcoes/funcoes.php
@@ -0,0 +1,20 @@
+<?php
+
+function connection(): PDO
+{
+    return new PDO(
+        'mysql:host=localhost;dbname=test',
+        'eddyxide',
+        'Eddyxidex!21'
+    );
+}
+
+function get_data($table): bool|array
+{
+    $connection = connection();
+    $query = $connection->query("SELECT * FROM {$table}");
+    $query->execute();
+    return $query->fetchAll();
+}
+
+var_dump(get_data('users'));
