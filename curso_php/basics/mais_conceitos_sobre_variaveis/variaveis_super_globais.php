8e130fc80404c55fdda2ecf1a6636a8d4c99cc27
Edson Pimenta <edson.tibo@gmail.com>
2022-11-04 14:56:39 -0300

    Introdução sobre variáveis super globais

---
+++ curso_php/basics/mais_conceitos_sobre_variaveis/variaveis_super_globais.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * As variáveis super globais são vistas em quaisquer
+ * partes do sistema. Quando construímos um projeto PHP,
+ * todas os nossos arquivos com extensão .php tem acesso
+ * às variáveis super globais.
+ */
+
+
+// $_COOKIE
+
+/*
+ * Um COOKIE é um valor guardado no navegador,
+ * que depois pode ser resgatado. Tod0 COOKIE
+ * tem uma data de validade, quando o criamos
+ * colocamos um prazo de validade.
+ */
+
+// $_SESSION
+
+/*
+ * Uma SESSION é guardada no navegador, mas não
+ * seu valor. É guardado um ID que, quando chamamos,
+ * pela SESSION nos retorna o valor guardado nela.
+ */
+
+// $_ENV
+
+/*
+ * Ainda não falaremos dele.
+ */
+
+// $_FILES
+
+/*
+ * Com o FILES podemos, por exemplo, pegar um
+ * arquivo enviado de um formulário.
+ */
+
+// $_GET
+
+/*
+ * Serve para pegar da URL as variáveis.
+ */
+
+// $_POST
+
+/*
+ * Com ela podemos pegar os valores enviados
+ * através do header.
+ */
+
+// $_REQUEST
+
+/*
+ * Com ela podemos pegar o que a $_GET e $_POST
+ * retornam.
+ */
+
+// $_SERVER
+
+/*
+ * Conseguimos pegar o tipo de requisição e a URI,
+ * por exemplo.
+ */
