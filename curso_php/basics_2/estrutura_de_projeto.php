e50528967cd0e7e4a75e239c64768c60dd99bfbb
Edson Pimenta <edson.tibo@gmail.com>
2022-11-09 11:25:38 -0300

    Anotações sobre estruturas de projeto

---
+++ curso_php/basics_2/estrutura_de_projeto.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Por onde começamos a estruturação de um projeto?
+ *
+ * Uma Estrutura bem criada, faz muita diferença.
+ *
+ * Geralmente criamos uma pasta "app" e, dentro dela,
+ * uma pasta chamada functions (isso se não trabalharmos
+ * com php orientado à objetos).
+ *
+ * Então, criamos uma pasta "public" com o arquivo "index.php".
+ *
+ * Quando uma pessoa entra em nosso site digitando:
+ *
+ * https://www.meusite.com.br
+ *
+ * O arquivo index.php na pasta public será o primeiro
+ * arquivo procurado.
+ *
+ * Além do "index.php", em "public" colocamos todos os
+ * nossos assets (os recursos do site, que são: CSS, JS,
+ * imagens, etc).
+ *
+ * Então, no diretório raiz do projeto, criamos o arquivo
+ * bootstrap.php, que seria o arquivo de inicialização. É
+ * nele que colaremos todas as funções e iniciaremos a sessão.
+ * O Bootstrap é a configuração do sistema, onde inicializamos
+ * o sistema.
+ */
