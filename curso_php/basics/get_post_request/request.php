2ed9960e654087d6e882740daff61e1068caf68d
Edson Pimenta <edson.tibo@gmail.com>
2023-01-11 10:31:02 -0300

    Anotações e exemplo sobre REQUEST

---
+++ curso_php/basics/get_post_request/request.php
@@ -0,0 +1,14 @@
+<?php
+
+/** REQUEST
+ * 
+ * Com o $_REQUEST temos uma funcionalidade
+ * diferente, pois ele pega ambos os valores
+ * enviados por GET e POST, além de mais alguns
+ * detalhes, como por exemplo o PHPSESSID e o
+ * token.
+ * 
+ * ALém disso, ele mostra os valores recebidos
+ * na URL, o que o torna menos seguro, assim
+ * como o GET ele mostra a query string na URL.
+ */
